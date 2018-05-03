a4532fe54792af2769c2a4dc34292b68efea7a68
Matheus Faria <matheus.sousa.faria@gmail.com>
2018-05-03 10:43:40 -0300

    Update docker to the new adpater v2

---
+++ .eslintrc.js
@@ -0,0 +1,298 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "accessor-pairs": "error",
+        "array-bracket-newline": "off",
+        "array-bracket-spacing": [
+            "error",
+            "never"
+        ],
+        "array-callback-return": "error",
+        "array-element-newline": "off",
+        "arrow-body-style": "error",
+        "arrow-parens": [
+            "error",
+            "as-needed"
+        ],
+        "arrow-spacing": [
+            "error",
+            {
+                "after": true,
+                "before": true
+            }
+        ],
+        "block-scoped-var": "off",
+        "block-spacing": [
+            "error",
+            "always"
+        ],
+        "brace-style": [
+            "error",
+            "1tbs",
+            {
+                "allowSingleLine": true
+            }
+        ],
+        "callback-return": "error",
+        "camelcase": "off",
+        "capitalized-comments": "off",
+        "class-methods-use-this": "off",
+        "comma-dangle": "error",
+        "comma-spacing": [
+            "error",
+            {
+                "after": true,
+                "before": false
+            }
+        ],
+        "comma-style": [
+            "error",
+            "last"
+        ],
+        "complexity": "error",
+        "computed-property-spacing": [
+            "error",
+            "never"
+        ],
+        "consistent-return": "off",
+        "consistent-this": "error",
+        "curly": "error",
+        "default-case": "off",
+        "dot-location": "error",
+        "dot-notation": "error",
+        "eol-last": "error",
+        "eqeqeq": "off",
+        "for-direction": "error",
+        "func-call-spacing": "error",
+        "func-name-matching": "error",
+        "func-names": [
+            "error",
+            "never"
+        ],
+        "func-style": "off",
+        "function-paren-newline": "off",
+        "generator-star-spacing": "error",
+        "getter-return": "error",
+        "global-require": "off",
+        "guard-for-in": "off",
+        "handle-callback-err": "error",
+        "id-blacklist": "error",
+        "id-length": "off",
+        "id-match": "error",
+        "implicit-arrow-linebreak": "off",
+        "indent": "off",
+        "indent-legacy": "off",
+        "init-declarations": "off",
+        "jsx-quotes": "error",
+        "key-spacing": "off",
+        "keyword-spacing": [
+            "error",
+            {
+                "after": true,
+                "before": true
+            }
+        ],
+        "line-comment-position": "error",
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "lines-around-comment": "error",
+        "lines-around-directive": "error",
+        "lines-between-class-members": "off",
+        "max-depth": "error",
+        "max-len": "off",
+        "max-lines": "error",
+        "max-nested-callbacks": "error",
+        "max-params": "off",
+        "max-statements": "off",
+        "max-statements-per-line": "off",
+        "multiline-comment-style": [
+            "error",
+            "starred-block"
+        ],
+        "new-cap": "error",
+        "new-parens": "error",
+        "newline-after-var": "off",
+        "newline-before-return": "off",
+        "newline-per-chained-call": "off",
+        "no-alert": "error",
+        "no-array-constructor": "error",
+        "no-await-in-loop": "error",
+        "no-bitwise": "error",
+        "no-buffer-constructor": "error",
+        "no-caller": "error",
+        "no-catch-shadow": "error",
+        "no-confusing-arrow": "error",
+        "no-continue": "off",
+        "no-div-regex": "error",
+        "no-duplicate-imports": "error",
+        "no-else-return": "off",
+        "no-empty-function": "error",
+        "no-eq-null": "off",
+        "no-eval": "error",
+        "no-extend-native": "error",
+        "no-extra-bind": "error",
+        "no-extra-label": "error",
+        "no-extra-parens": "off",
+        "no-floating-decimal": "error",
+        "no-implicit-globals": "error",
+        "no-implied-eval": "error",
+        "no-inline-comments": "error",
+        "no-inner-declarations": [
+            "error",
+            "functions"
+        ],
+        "no-invalid-this": "off",
+        "no-iterator": "error",
+        "no-label-var": "error",
+        "no-labels": "error",
+        "no-lone-blocks": "error",
+        "no-lonely-if": "off",
+        "no-loop-func": "off",
+        "no-magic-numbers": "off",
+        "no-mixed-operators": "error",
+        "no-mixed-requires": "error",
+        "no-multi-assign": "error",
+        "no-multi-spaces": "error",
+        "no-multi-str": "error",
+        "no-multiple-empty-lines": "error",
+        "no-native-reassign": "error",
+        "no-negated-condition": "off",
+        "no-negated-in-lhs": "error",
+        "no-nested-ternary": "error",
+        "no-new": "error",
+        "no-new-func": "error",
+        "no-new-object": "error",
+        "no-new-require": "error",
+        "no-new-wrappers": "error",
+        "no-octal-escape": "error",
+        "no-param-reassign": "off",
+        "no-path-concat": "error",
+        "no-plusplus": "off",
+        "no-process-env": "off",
+        "no-process-exit": "off",
+        "no-proto": "error",
+        "no-prototype-builtins": "error",
+        "no-restricted-globals": "error",
+        "no-restricted-imports": "error",
+        "no-restricted-modules": "error",
+        "no-restricted-properties": "error",
+        "no-restricted-syntax": "error",
+        "no-return-assign": "off",
+        "no-return-await": "error",
+        "no-script-url": "error",
+        "no-self-compare": "error",
+        "no-sequences": "error",
+        "no-shadow": "off",
+        "no-shadow-restricted-names": "error",
+        "no-spaced-func": "error",
+        "no-sync": "off",
+        "no-tabs": "error",
+        "no-template-curly-in-string": "error",
+        "no-ternary": "off",
+        "no-throw-literal": "error",
+        "no-trailing-spaces": "error",
+        "no-undef-init": "off",
+        "no-undefined": "off",
+        "no-underscore-dangle": "off",
+        "no-unmodified-loop-condition": "error",
+        "no-unneeded-ternary": "error",
+        "no-unused-expressions": "error",
+        "no-use-before-define": "off",
+        "no-useless-call": "error",
+        "no-useless-computed-key": "error",
+        "no-useless-concat": "error",
+        "no-useless-constructor": "error",
+        "no-useless-rename": "error",
+        "no-useless-return": "error",
+        "no-var": "off",
+        "no-void": "error",
+        "no-warning-comments": "off",
+        "no-whitespace-before-property": "error",
+        "no-with": "error",
+        "nonblock-statement-body-position": "error",
+        "object-curly-newline": "off",
+        "object-curly-spacing": [
+            "error",
+            "always"
+        ],
+        "object-shorthand": "error",
+        "one-var": "off",
+        "one-var-declaration-per-line": [
+            "error",
+            "initializations"
+        ],
+        "operator-assignment": "error",
+        "operator-linebreak": "error",
+        "padded-blocks": "off",
+        "padding-line-between-statements": "error",
+        "prefer-arrow-callback": "off",
+        "prefer-const": "off",
+        "prefer-destructuring": "off",
+        "prefer-numeric-literals": "error",
+        "prefer-promise-reject-errors": "error",
+        "prefer-reflect": "off",
+        "prefer-rest-params": "error",
+        "prefer-spread": "error",
+        "prefer-template": "off",
+        "quote-props": "off",
+        "quotes": "off",
+        "radix": "error",
+        "require-await": "error",
+        "require-jsdoc": "off",
+        "rest-spread-spacing": "error",
+        "semi": "error",
+        "semi-spacing": [
+            "error",
+            {
+                "after": true,
+                "before": false
+            }
+        ],
+        "semi-style": "off",
+        "sort-imports": "error",
+        "sort-keys": "off",
+        "sort-vars": "error",
+        "space-before-blocks": "error",
+        "space-before-function-paren": "off",
+        "space-in-parens": "off",
+        "space-infix-ops": "error",
+        "space-unary-ops": [
+            "error",
+            {
+                "nonwords": false,
+                "words": true
+            }
+        ],
+        "spaced-comment": "off",
+        "strict": "error",
+        "switch-colon-spacing": "error",
+        "symbol-description": "error",
+        "template-curly-spacing": [
+            "error",
+            "never"
+        ],
+        "template-tag-spacing": "error",
+        "unicode-bom": [
+            "error",
+            "never"
+        ],
+        "valid-jsdoc": "error",
+        "vars-on-top": "off",
+        "wrap-iife": "error",
+        "wrap-regex": "error",
+        "yield-star-spacing": "error",
+        "yoda": [
+            "error",
+            "never"
+        ]
+    }
+};
--- Dockerfile
+++ Dockerfile
@@ -2,52 +2,32 @@ FROM node:8-alpine
 
 LABEL mantainer "Diego Dorgam <diego.dorgam@rocket.chat>"
 
-ENV NATURAL_LANG='en'                                                  \
-    NATURAL_CORPUS='training_data/corpus.yml'                          \
-    HUBOT_ADAPTER=rocketchat                                         \
-    HUBOT_OWNER=RocketChat                                           \
-    HUBOT_NAME=HubotNatural                                          \
-    HUBOT_DESCRIPTION="Processamento de linguagem natural com hubot" \
-    HUBOT_LOG_LEVEL=debug                                            \
-    ROCKETCHAT_URL=http://rocketchat:3000                            \
-    ROCKETCHAT_USESSL='false'                                         \
-    ROCKETCHAT_ROOM=GENERAL                                          \
-    ROCKETCHAT_USER=bot-username                                          \
-    ROCKETCHAT_PASSWORD=bot-password                                     \
-    ROCKETCHAT_AUTH=password                                         \
-    RESPOND_TO_DM=true                                               \
-    RESPOND_TO_LIVECHAT=true                                         \
-    RESPOND_TO_EDITED=true                                           \
+ENV NATURAL_LANG='en'                                           \
+    NATURAL_CORPUS='training_data/corpus.yml'                   \
+    HUBOT_ADAPTER=rocketchat                                    \
+    HUBOT_OWNER=RocketChat                                      \
+    HUBOT_NAME=HubotNatural                                     \
+    HUBOT_DESCRIPTION="Natural Language Processing with hubot"  \
+    HUBOT_LOG_LEVEL=debug                                       \
+    ROCKETCHAT_URL=http://rocketchat:3000                       \
+    ROCKETCHAT_USESSL='false'                                   \
+    ROCKETCHAT_ROOM=GENERAL                                     \
+    ROCKETCHAT_USER=bot-username                                \
+    ROCKETCHAT_PASSWORD=bot-password                            \
+    ROCKETCHAT_AUTH=password                                    \
+    RESPOND_TO_DM=true                                          \
+    RESPOND_TO_LIVECHAT=true                                    \
+    RESPOND_TO_EDITED=true                                      \
     LISTEN_ON_ALL_PUBLIC=true
 
-RUN apk --update add --no-cache git make gcc g++ python python-dev && \
-    addgroup -S hubotnat && adduser -S -g hubotnat hubotnat
 
-RUN npm install -g yo generator-hubot@1.0.0 node-gyp
+workdir hubotnatural
 
-WORKDIR /home/hubotnat/bot
+add . /hubotnatural
 
-USER root
+run apk --update add --no-cache --virtual build-dependencies    \
+                     git make gcc g++ python                 && \
+    npm install                                              && \
+    apk del build-dependencies
 
-RUN mkdir -p /home/hubotnat/.config/configstore                             && \
-    echo "optOut: true" > /home/hubotnat/.config/configstore/insight-yo.yml && \
-    chown -R hubotnat:hubotnat /home/hubotnat
-
-USER hubotnat
-
-RUN yo hubot --adapter ${HUBOT_ADAPTER}         \
-             --owner ${HUBOT_OWNER}             \
-             --name ${HUBOT_NAME}               \
-             --description ${HUBOT_DESCRIPTION} \
-             --defaults --no-insight         && \
-    rm /home/hubotnat/bot/external-scripts.json
-
-COPY ["package.json", "/home/hubotnat/bot/"]
-
-ADD scripts/ /home/hubotnat/bot/scripts/
-
-ADD training_data/ /home/hubotnat/bot/training_data
-
-RUN npm install --save
-
-ENTRYPOINT /home/hubotnat/bot/bin/hubot -a ${HUBOT_ADAPTER} -n ${HUBOT_NAME} -l ${ROCKETCHAT_USER}
+entrypoint bin/hubot
